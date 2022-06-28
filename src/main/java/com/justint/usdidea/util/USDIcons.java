7abcca03a5df526ea0e703963b062a58e4aec841
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-28 15:53:40 +0200

    switch to not deprecated method

--- src/main/java/com/justint/usdidea/util/USDIcons.java
+++ src/main/java/com/justint/usdidea/util/USDIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class USDIcons {
-    public static final Icon FILE = IconLoader.getIcon("/com/justint/usdidea/icons/usd-file.svg");
+    public static final Icon FILE = IconLoader.getIcon("/com/justint/usdidea/icons/usd-file.svg", USDIcons.class);
 }
